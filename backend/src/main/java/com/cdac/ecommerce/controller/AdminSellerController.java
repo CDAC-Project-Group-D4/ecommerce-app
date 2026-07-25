ed297d8bb068a5869fd0a070443e0bb0dc2fdb8e
Shreyansh Bhardwaj <shreyansh272002@gmail.com>
2026-07-25 21:32:52 +0530

    admin seller controller added

---
+++ backend/src/main/java/com/cdac/ecommerce/controller/AdminSellerController.java
@@ -0,0 +1,33 @@
+package com.cdac.ecommerce.controller;
+
+import com.cdac.ecommerce.dto.response.SellerResponseDTO;
+import com.cdac.ecommerce.service.AdminSellerService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/admin/sellers")
+@RequiredArgsConstructor
+public class AdminSellerController {
+
+    private final AdminSellerService adminSellerService;
+
+    @GetMapping
+    @Operation(summary = "Get all sellers")
+    public ResponseEntity<List<SellerResponseDTO>> getAllSellers(){
+
+        List<SellerResponseDTO> sellerDTOS = adminSellerService.getAllSellers();
+        return new ResponseEntity<>(sellerDTOS, HttpStatus.OK);
+    }
+
+
+
+
+}
