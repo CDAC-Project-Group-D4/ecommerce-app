3cdfbc90bfb97a444ca3e4156a2863d0214b5069
Akanksha More <amore4969@gmail.com>
2026-07-27 23:45:11 +0530

    Resolved Category.java merge conflict

--- backend/src/main/java/com/cdac/ecommerce/entity/Category.java
+++ backend/src/main/java/com/cdac/ecommerce/entity/Category.java
@@ -11,24 +11,20 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
+//@ToString
 @Table(name = "categories")
 @AttributeOverride(name="category_id",column = @Column(name="id"))
 public class Category extends BaseClass{
 
-    @Column(name="category_name", nullable = false, unique = true)
-    private String name;
-
-    @Column(name="is_active", nullable = false)
-    private boolean isActive = true;
-
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
     private Category parent;
 
-    //self - referencing
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
-    private List<Category> subCategories = new ArrayList<>();
+    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL)
+    List<Product> products=new ArrayList<>();
 
-    @OneToMany(mappedBy = "category")
-    private List<Product> products = new ArrayList<>();
+    private String name;
+
+    private boolean isActive = true;
 
 }
