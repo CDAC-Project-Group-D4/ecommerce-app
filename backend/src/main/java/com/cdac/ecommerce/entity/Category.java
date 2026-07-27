5c259022d0da1c3b21c9f8f9bdba38eba7389523
Akanksha More <amore4969@gmail.com>
2026-07-28 00:14:17 +0530

    Entity category added

--- backend/src/main/java/com/cdac/ecommerce/entity/Category.java
+++ backend/src/main/java/com/cdac/ecommerce/entity/Category.java
@@ -16,15 +16,21 @@ import java.util.List;
 @AttributeOverride(name="category_id",column = @Column(name="id"))
 public class Category extends BaseClass{
 
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
-    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL)
+//    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL)
+
+    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL , orphanRemoval = true)
+
     List<Product> products=new ArrayList<>();
 
     private String name;
 
+    @Column(name = "is_active", nullable = false)
     private boolean isActive = true;
+
 
 }
