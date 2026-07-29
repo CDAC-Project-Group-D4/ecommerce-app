a66b1eb132be7085cbdac693b8901f833437ead1
Akanksha More <amore4969@gmail.com>
2026-07-29 10:36:10 +0530

    Customer Product Mapper

--- backend/src/main/java/com/cdac/ecommerce/mapper/CustomerProductMapper.java
+++ backend/src/main/java/com/cdac/ecommerce/mapper/CustomerProductMapper.java
@@ -1,6 +1,7 @@
 package com.cdac.ecommerce.mapper;
 
 import com.cdac.ecommerce.dto.response.CustomerProductResponseDTO;
+import com.cdac.ecommerce.dto.response.ProductCardDTO;
 import com.cdac.ecommerce.entity.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -13,4 +14,12 @@ public interface CustomerProductMapper {
     @Mapping(target="categoryId",source="category.id")
     CustomerProductResponseDTO toDto(Product product);
 
+    // Product Listing API
+//    @Mapping(target = "id", source = "id")
+//    @Mapping(target = "name", source = "name")
+//    @Mapping(target = "storeName", source = "store.storeName")
+////    @Mapping(target = "thumbnailUrl", source = "thumbnailUrl")
+//    ProductCardDTO toCardDTO(Product product);
+        @Mapping(target = "storeName", source = "store.storeName")
+        ProductCardDTO toCardDTO(Product product);
 }
