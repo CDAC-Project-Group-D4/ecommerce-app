9ac7aa4c5f6ef57f83c284867977d4c1ae0ac51a
Akanksha More <amore4969@gmail.com>
2026-07-29 10:37:54 +0530

    Customer Address Repository added

--- backend/src/main/java/com/cdac/ecommerce/repository/CustomerAddressRepository.java
+++ backend/src/main/java/com/cdac/ecommerce/repository/CustomerAddressRepository.java
@@ -1,13 +1,14 @@
 package com.cdac.ecommerce.repository;
 
 import com.cdac.ecommerce.entity.CustomerAddress;
+import com.cdac.ecommerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, Long> {
-
+    List<CustomerAddress> findByUser(User user);
     List<CustomerAddress> findByUser_Id(Long userId);
 
     List<CustomerAddress> findByUser_IdAndActiveTrue(Long userId);
