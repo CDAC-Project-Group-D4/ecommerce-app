581b9d69474890dcc8350c70c26eb524e0ca5209
Shreyansh Bhardwaj <shreyansh272002@gmail.com>
2026-08-03 21:52:36 +0530

    added a method to convert to return request response dto that also contains images

--- backend/src/main/java/com/cdac/ecommerce/mapper/ReturnMapper.java
+++ backend/src/main/java/com/cdac/ecommerce/mapper/ReturnMapper.java
@@ -1,9 +1,16 @@
 package com.cdac.ecommerce.mapper;
 
+import com.cdac.ecommerce.dto.response.ReturnRequestImageResponseDTO;
+import com.cdac.ecommerce.dto.response.ReturnRequestResponseDTO;
 import com.cdac.ecommerce.dto.response.ReturnResponseDTO;
 import com.cdac.ecommerce.entity.OrderItem;
 import com.cdac.ecommerce.entity.ReturnRequest;
+import com.cdac.ecommerce.entity.ReturnRequestImage;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 public class ReturnMapper {
@@ -36,5 +43,42 @@ public class ReturnMapper {
         }
 
         return dto;
+    }
+
+    public ReturnRequestResponseDTO toDtoWithImage(ReturnRequest request){
+
+        if(request == null){
+            return null;
+        }
+
+        List<ReturnRequestImageResponseDTO> imageResponseDTOList = request.getImages() != null
+                ? request.getImages().stream()
+                .map(this::toImageDto)
+                .toList()
+                : Collections.emptyList();
+
+        return ReturnRequestResponseDTO.builder()
+                .id(request.getId())
+                .orderId(request.getOrder() != null ? request.getOrder().getId() : null)
+                .orderItemId(request.getOrderItem() != null ? request.getOrderItem().getId() : null)
+                .requestType(request.getRequestType() != null ? request.getRequestType().name() : null)
+                .sellerDecision(request.getSellerDecision() != null ? request.getSellerDecision().name() : null)
+                .adminDecision(request.getAdminDecision() != null ? request.getAdminDecision().name() : null)
+                .images(imageResponseDTOList)
+                .createdAt(request.getCreatedAt())
+                .build();
+
+    }
+
+    public ReturnRequestImageResponseDTO toImageDto(ReturnRequestImage image){
+        if(image == null){
+            return null;
+        }
+
+        return ReturnRequestImageResponseDTO.builder()
+                .id(image.getId())
+                .imageUrl(image.getImageUrl())
+                .createdAt(image.getCreatedAt())
+                .build();
     }
 }
