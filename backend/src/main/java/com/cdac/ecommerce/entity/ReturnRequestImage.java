aca1d22c26b1485d95c12dae749454ae7e157932
Shreyansh Bhardwaj <shreyansh272002@gmail.com>
2026-08-03 21:59:51 +0530

    added return request image entity

---
+++ backend/src/main/java/com/cdac/ecommerce/entity/ReturnRequestImage.java
@@ -0,0 +1,36 @@
+package com.cdac.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "review_request_images")
+public class ReturnRequestImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "return_request_id", nullable = false)
+    private ReturnRequest returnRequest;
+
+    @Column(name = "image_url", nullable = false, length = 500)
+    private String imageUrl;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+
+    }
+}
